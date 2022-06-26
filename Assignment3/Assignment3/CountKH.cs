eb3378439d139434f3641f63deeb4f9ef6b1ce27
lcisme <92872523+lcisme@users.noreply.github.com>
2022-06-26 20:52:13 +0700

    Update CountKH.cs

--- Assignment3/Assignment3/CountKH.cs
+++ Assignment3/Assignment3/CountKH.cs
@@ -1,12 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Assignment3
 {
-    internal class CountKH
+    public class CountKH
     {
+        ArrayList slknn = new ArrayList();
+        ArrayList slkvn = new ArrayList();
+
+
+        public int slkhachvn { get; set; }
+        public int slkhachnn { get; set; }
+
+        public void demkvn(int soluong)
+        {
+            slkvn.Add(soluong);
+        }
+        public void demknn(int soluong)
+        {
+            slknn.Add(soluong);
+        }
+
+        public int demkhachvn()
+        {
+            int dem = 0;
+            if (slkvn.Capacity == 0)
+            {
+                dem = 0;
+            }
+            else
+            {
+               foreach(int check in slkvn)
+                {
+                    dem += 1;
+                }
+            }
+            return slkhachvn = dem;
+        }
+
+        public int demkhachnn()
+        {
+            int dem = 0;
+            if (slknn.Capacity == 0)
+            {
+                dem = 0;
+            }
+            else
+            {
+                foreach (int check in slknn)
+                {
+                    dem += 1;
+                }
+            }
+            return slkhachnn = dem;
+        }
     }
 }
