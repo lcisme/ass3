ca3a23e11d6fee1bf16a9b5f87565c3085305fef
lcisme <92872523+lcisme@users.noreply.github.com>
2022-06-26 20:52:34 +0700

    Update TBTienKhachNN.cs

--- Assignment3/Assignment3/TBTienKhachNN.cs
+++ Assignment3/Assignment3/TBTienKhachNN.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Assignment3
 {
     public class TBTienKhachNN
     {
-            List<Double> dsknn = new List<Double>();
+            ArrayList dsknn = new ArrayList();
             
 
             public double tientb { get; set; }
@@ -16,6 +17,22 @@ namespace Assignment3
             public void slkh(double giatien)
         {
             dsknn.Add(giatien);
+        }
+
+        public double giatientb() {
+            double tong = 0;
+            if(dsknn.Capacity == 0)
+            {
+                tong = 0;
+            }
+            else
+            {
+                foreach (double dem in dsknn)
+                {
+                    tong = tong + dem;
+                }
+            }
+            return tientb = tong / dsknn.Count;
         }
     }
 }
